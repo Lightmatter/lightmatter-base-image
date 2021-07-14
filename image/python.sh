96a81bb085e87bc8ed07861a1a6783aeafd4f693
C-alcium <62979683+C-alcium@users.noreply.github.com>
2021-07-14 12:15:36 +0100

    Add first missing package

--- image/python.sh
+++ image/python.sh
@@ -8,4 +8,5 @@ header "Installing Python..."
 minimal_apt_get_install python python2.7 python3.8 python3.8-dev python3-pip python3-setuptools
 #precache uvloop
 python3.8 -m pip install wheel
+python3.8 -m pip install cffi
 python3.8 -m pip install uvicorn==0.11.3 gunicorn==20.0.4
